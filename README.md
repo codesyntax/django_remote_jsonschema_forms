955dd96f7574de311a676b30995d840df5d30f5a
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-06-11 10:31:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# django_remote_jsonschema_forms
+
+
+A package that allows you to serialize django forms, including fields and widgets into Python
+dictionary for easy conversion into JSON Schema.
+
+This package is heavily based on [django-remote-forms](https://github.com/WiserTogether/django-remote-forms)
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+
+    use_setuptools()
+    from setuptools import setup
+
+setup(
+    name="django_remote_jsonschema_forms",
+    version="1.0.0",
+    description="A platform independent form serializer for Django.",
+    author="Gorka Garcia",
+    author_email="ggarcia@codesyntax.com",
+    url="http://github.com/codesyntax/django_remote_jsonschema_forms/",
+    long_description=open("README.md", "r").read(),
+    packages=[
+        "django_remote_jsonschema_forms",
+    ],
+    package_data={},
+    zip_safe=False,
+    requires=[],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: Pre Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: MIT",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+    ],
+)
