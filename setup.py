41907d19d41aa0affc8412f496ac8f1dde8614fa
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-06-19 08:53:04 +0200

    set supported python versions

--- setup.py
+++ setup.py
@@ -32,6 +32,10 @@ setup(
         "License :: MIT",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Utilities",
     ],
 )
