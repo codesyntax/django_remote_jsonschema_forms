07e068cc89a370f9f6ec5f04ac7320f6e16a5532
Urtzi Odriozola <uodriozola@codesyntax.com>
2025-05-07 15:26:48 +0200

    Update changelog

--- setup.py
+++ setup.py
@@ -1,31 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-try:
-    from setuptools import setup
-except ImportError:
-    from ez_setup import use_setuptools
+from setuptools import setup
 
-    use_setuptools()
-    from setuptools import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="django_remote_jsonschema_forms",
     version="1.0.0",
     description="A platform independent form serializer for Django.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Gorka Garcia",
     author_email="ggarcia@codesyntax.com",
-    url="http://github.com/codesyntax/django_remote_jsonschema_forms/",
-    long_description=open("README.md", "r").read(),
-    long_description_content_type='text/markdown',
-    keywords="jsonschema,react,django,python",
-    python_requires='>=3.9',
-    packages=[
-        "django_remote_jsonschema_forms",
-    ],
-    package_data={},
+    url="https://github.com/codesyntax/django_remote_jsonschema_forms",
+    packages=["django_remote_jsonschema_forms"],
+    python_requires=">=3.9",
+    install_requires=[],  # Zure benetako dependentziak hemen jartzea gomendatzen da
     zip_safe=False,
-    requires=[],
-    install_requires=[],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
@@ -39,4 +31,6 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Topic :: Utilities",
     ],
+    keywords="jsonschema,react,django,python",
 )
+
